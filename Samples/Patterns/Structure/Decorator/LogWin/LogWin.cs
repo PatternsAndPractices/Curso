2aea1ce473cb20c6f35999574b00ca9c9a0fedd6
Jucer74 <jucer74@hotmail.com>
2018-11-18 22:03:38 -0500

    Nota

--- Samples/Patterns/Structure/Decorator/LogWin/LogWin.cs
+++ Samples/Patterns/Structure/Decorator/LogWin/LogWin.cs
@@ -10,7 +10,6 @@ namespace PnP.Patterns.Structure.Decorator
         public LogWin(ILoad component) : base(component)
         {
             EventLog evt = new EventLog();
-
             // NOTA: PARA QUE ESTA INSTRUCCION FUNCIONES SE DEBE
             // EJECUTAR VISUAL STUDIO EN MODO ADMINISTRADOR
             if (!EventLog.SourceExists(SourceName))
